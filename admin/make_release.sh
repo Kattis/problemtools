f90c773231f4225864dad1c69eb0e6e9ae9c8103
Per Austrin <austrin@kattis.com>
2016-07-22 18:57:05 +0200

    change version numbering scheme slightly

--- admin/make_release.sh
+++ admin/make_release.sh
@@ -3,11 +3,11 @@
 set -e
 
 ROOT=$(readlink -f $(dirname $0)/..)
-VERSION=$(date +%Y%m%d)
+VERSION=1.$(date +%Y%m%d)
 
-if [ "$(git tag -l $VERSION)" != "" ]; then
+if [ "$(git tag -l v$VERSION)" != "" ]; then
     REV=1
-    while [ "$(git tag -l $VERSION-rev$REV)" != "" ]; do
+    while [ "$(git tag -l v$VERSION-rev$REV)" != "" ]; do
         REV=$((REV+1))
     done
     VERSION=$VERSION-rev$REV
@@ -21,7 +21,7 @@ git flow release start $VERSION
 $ROOT/admin/update_version.py.sh $VERSION
 
 # Update debian/changelog
-git-dch $ROOT --release --new-version=$VERSION --git-author --debian-tag='%(version)s' --debian-branch=release/$VERSION --spawn-editor=never
+git-dch $ROOT --release --new-version=$VERSION --git-author --debian-tag='v%(version)s' --debian-branch=release/$VERSION --spawn-editor=never
 
 git add $ROOT/problemtools/_version.py $ROOT/debian/changelog
 git commit -m "Release of version $VERSION: bump version in problemtools/_version.py and debian/changelog"
