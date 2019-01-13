2a0e3a79eb5af9f89cb2f83d008fec002140dd4a
Tobias Werth <werth@cs.fau.de>
2019-01-13 17:24:13 +0100

    Add more submissions for the interactive example problem.

---
+++ examples/guess/submissions/time_limit_exceeded/guess_tle_after_correct.cc
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstring>
+
+int main(void) {
+    int lo = 1, hi = 1000;
+    while (true) {
+        int m = (lo+hi)/2;
+        printf("%d\n", m);
+        fflush(stdout);
+        char res[1000];
+        scanf("%s", res);
+        if (!strcmp(res, "correct")) break;
+        if (!strcmp(res, "lower")) hi = m-1;
+        else lo = m+1;
+    }
+    if ((lo+hi)/2 > 666) {
+	    fclose(stdout);
+	    while (true) { /* never exit */ }
+    }
+    return 0;
+}
