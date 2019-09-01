Kattis/problemtools

commit bdd588e52a849ebee7da5768fe1a7e8609cb3dd3

remove flawed Template class destructor by making Template a context manager

problemtools/problem2pdf.py
@@ -22,38 +22,33 @@ def convert(problem, options=None):
 
     texfile = problem
     # Set up template if necessary
-    templ = None
-    if os.path.isdir(problem):
-        templ = template.Template(problem, language=options.language,
-                                  title=options.title)
+    with template.Template(problem, language=options.language,
+                           title=options.title) as templ:
         texfile = templ.get_file_name()
 
-    origcwd = os.getcwd()
+        origcwd = os.getcwd()
 
-    os.chdir(os.path.dirname(texfile))
-    params = ['pdflatex', '-interaction=nonstopmode']
-    output = None
-    if options.quiet:
-        output = open(os.devnull, 'w')
-    if options.nopdf:
-        params.append('-draftmode')
+        os.chdir(os.path.dirname(texfile))
+        params = ['pdflatex', '-interaction=nonstopmode']
+        output = None
+        if options.quiet:
+            output = open(os.devnull, 'w')
+        if options.nopdf:
+            params.append('-draftmode')
 
-    params.append(texfile)
+        params.append(texfile)
 
-    status = subprocess.call(params, stdout=output)
-    if status == 0:
         status = subprocess.call(params, stdout=output)
+        if status == 0:
+            status = subprocess.call(params, stdout=output)
 
-    if output is not None:
-        output.close()
+        if output is not None:
+            output.close()
 
-    os.chdir(origcwd)
+        os.chdir(origcwd)
 
-    if not options.nopdf:
-        shutil.move(os.path.splitext(texfile)[0] + '.pdf', destfile)
-
-    if templ != None:
-        templ.cleanup()
+        if not options.nopdf:
+            shutil.move(os.path.splitext(texfile)[0] + '.pdf', destfile)
 
     return status == 0
 

problemtools/template.py
@@ -16,10 +16,6 @@ def detect_version(problemdir, problemtex):
 
 
 class Template:
-    filename = None
-    problemset_cls = None
-    copy_cls = True
-
     def __init__(self, problemdir, language='',
                  title='Problem Title', force_copy_cls=False):
         if not os.path.isdir(problemdir):
@@ -50,65 +46,64 @@ class Template:
             dotlang = '.' + language
 
         # Used in the template.tex variable substitution.
-        language = dotlang
+        self.language = dotlang
         problemtex = os.path.join(stmtdir, 'problem' + dotlang + '.tex')
 
         if not os.path.isfile(problemtex):
             raise Exception('Unable to find problem statement, was looking for "%s"' % problemtex)
-
-        templatefile = 'template.tex'
-        clsfile = 'problemset.cls'
+        
+        self.templatefile = 'template.tex'
+        self.clsfile = 'problemset.cls'
         timelim = 1  # Legacy for compatibility with v0.1
         version = detect_version(problemdir, problemtex)
         if version != '':
             print('Note: problem is in an old version (%s) of problem format, you should consider updating it' % version)
-            templatefile = 'template_%s.tex' % version
-            clsfile = 'problemset_%s.cls' % version
+            self.templatefile = 'template_%s.tex' % version
+            self.clsfile = 'problemset_%s.cls' % version
 
         templatepaths = [os.path.join(os.path.dirname(__file__), 'templates/latex'),
                          os.path.join(os.path.dirname(__file__), '../templates/latex'),
                          '/usr/lib/problemtools/templates/latex']
-        templatepath = None
-        for p in templatepaths:
-            if os.path.isdir(p) and os.path.isfile(os.path.join(p, templatefile)):
-                templatepath = p
-                break
+        self.templatepath = next((p for p in templatepaths
+                                  if os.path.isdir(p) and os.path.isfile(os.path.join(p, self.templatefile))),
+                                 None)
+        if self.templatepath is None:
+            raise Exception('Could not find directory with latex template "%s"' % self.templatefile)
 
-        if templatepath == None:
-            raise Exception('Could not find directory with latex template "%s"' % templatefile)
+        self.basedir = os.path.dirname(problemdir)
+        self.shortname = os.path.basename(problemdir)
+        self.samples = [os.path.splitext(os.path.basename(f))[0] for f in sorted(glob.glob(os.path.join(problemdir, 'data', 'sample', '*.in')))]
+        self.problemset_cls = os.path.join(self.basedir, 'problemset.cls')
 
-        basedir = os.path.dirname(problemdir)
-        shortname = os.path.basename(problemdir)
-        samples = [os.path.splitext(os.path.basename(f))[0] for f in sorted(glob.glob(os.path.join(problemdir, 'data', 'sample', '*.in')))]
-        self.problemset_cls = os.path.join(basedir, 'problemset.cls')
-
+        self.copy_cls = True
         if os.path.isfile(self.problemset_cls) and not force_copy_cls:
             print('%s exists, will not copy it -- in case of weirdness this is likely culprit' % self.problemset_cls)
             self.copy_cls = False
 
-        if self.copy_cls:
-            shutil.copyfile(os.path.join(templatepath, clsfile), self.problemset_cls)
 
-        (templfd, self.filename) = tempfile.mkstemp(suffix='.tex', dir=basedir)
+    def __enter__(self):
+        if self.copy_cls:
+            shutil.copyfile(os.path.join(self.templatepath, self.clsfile), self.problemset_cls)
+
+        (templfd, self.filename) = tempfile.mkstemp(suffix='.tex', dir=self.basedir)
         templout = os.fdopen(templfd, 'w')
-        templin = open(os.path.join(templatepath, templatefile))
+        templin = open(os.path.join(self.templatepath, self.templatefile))
+        data = {'language': self.language,
+                'shortname': self.shortname}
         for line in templin:
             try:
-                out = line % locals()
-                templout.write(out)
+                templout.write(line % data)
             except:
                 # This is a bit ugly I guess
-                for sample in samples:
-                    out = line % locals()
-                    templout.write(out)
+                for sample in self.samples:
+                    data['sample'] = sample
+                    templout.write(line % data)
+                del data['sample']
         templout.close()
         templin.close()
+        return self
 
-    def get_file_name(self):
-        assert os.path.isfile(self.filename)
-        return self.filename
-
-    def cleanup(self):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
         if self.problemset_cls is not None and self.copy_cls and  os.path.isfile(self.problemset_cls):
             os.remove(self.problemset_cls)
         if self.filename is not None:
@@ -116,5 +111,6 @@ class Template:
                 if os.path.isfile(f):
                     os.remove(f)
 
-    def __del__(self):
-        self.cleanup()
+    def get_file_name(self):
+        assert os.path.isfile(self.filename)
+        return self.filename

problemtools/problem2html.py
@@ -32,38 +32,36 @@ def convert(problem, options=None):
 
     texfile = problem
     # Set up template if necessary
-    templ = None
-    if os.path.isdir(problem):
-        templ = template.Template(problem, language=options.language, title=options.title)
+    with template.Template(problem, language=options.language, title=options.title) as templ:
         texfile = open(templ.get_file_name(), 'r')
 
-    origcwd = os.getcwd()
+        origcwd = os.getcwd()
 
-    # Setup parser and renderer etc
+        # Setup parser and renderer etc
 
-    # Python 2 compatibility: the second keyword argument for th TeX
-    # class changed name from file to myfile in Python 3 version.  When
-    # Python 2 compatibility is dropped, change this to "tex =
-    # TeX(myfile=texfile)".
-    tex = TeX(None, texfile)
+        # Python 2 compatibility: the second keyword argument for th TeX
+        # class changed name from file to myfile in Python 3 version.  When
+        # Python 2 compatibility is dropped, change this to "tex =
+        # TeX(myfile=texfile)".
+        tex = TeX(None, texfile)
 
-    ProblemsetMacros.init(tex)
+        ProblemsetMacros.init(tex)
 
-    tex.ownerDocument.config['general']['copy-theme-extras'] = options.css
-    if not options.headers:
-        tex.ownerDocument.userdata['noheaders'] = True
-    tex.ownerDocument.config['files']['filename'] = destfile
-    tex.ownerDocument.config['images']['filenames'] = 'img-$num(4)'
-    tex.ownerDocument.config['images']['enabled'] = False
-    tex.ownerDocument.config['images']['imager'] = 'none'
-    tex.ownerDocument.config['images']['base-url'] = imgbasedir
+        tex.ownerDocument.config['general']['copy-theme-extras'] = options.css
+        if not options.headers:
+            tex.ownerDocument.userdata['noheaders'] = True
+        tex.ownerDocument.config['files']['filename'] = destfile
+        tex.ownerDocument.config['images']['filenames'] = 'img-$num(4)'
+        tex.ownerDocument.config['images']['enabled'] = False
+        tex.ownerDocument.config['images']['imager'] = 'none'
+        tex.ownerDocument.config['images']['base-url'] = imgbasedir
 
-    renderer = ProblemRenderer()
+        renderer = ProblemRenderer()
 
-    if not options.quiet:
-        print('Parsing TeX source...')
-    doc = tex.parse()
-    texfile.close()
+        if not options.quiet:
+            print('Parsing TeX source...')
+        doc = tex.parse()
+        texfile.close()
 
     # Go to destdir
     if destdir:
@@ -97,8 +95,6 @@ def convert(problem, options=None):
     finally:
         # restore cwd
         os.chdir(origcwd)
-        if templ:
-            templ.cleanup()
 
     return True
 

commit a2535dcbcc949c88feb785fc1a738580dc2bdfe3

fix two small template-related bugs introduced in bdd588e5

problemtools/problem2pdf.py
@@ -47,7 +47,7 @@ def convert(problem, options=None):
 
         os.chdir(origcwd)
 
-        if not options.nopdf:
+        if status == 0 and not options.nopdf:
             shutil.move(os.path.splitext(texfile)[0] + '.pdf', destfile)
 
     return status == 0

problemtools/template.py
@@ -101,7 +101,8 @@ class Template:
                 for sample in self.samples:
                     data['sample'] = sample
                     templout.write(line % data)
-                del data['sample']
+                if self.samples:
+                    del data['sample']
         templout.close()
         templin.close()
         return self