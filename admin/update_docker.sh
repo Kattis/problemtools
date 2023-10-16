2449dbbfc42d16639c4b83f3880a7fdb7ddf4ec3
Per Austrin <austrin@kattis.com>
2023-10-16 14:23:43 +0200

    [docker] update docker image files and build script

--- admin/update_docker.sh
+++ admin/update_docker.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-KOTLIN_VERSION=1.3.50
-
-
 TAG=develop
 UPDATE_LATEST=false
 if [ "$1" != "" ]; then
@@ -28,16 +25,6 @@ rm -rf artifacts/deb/*
 sudo docker run --rm -v "$(pwd)/artifacts/:/artifacts" problemtools/build:${TAG} cp -r /usr/local/problemtools_build/deb /artifacts
 sudo chown -R $USER:$USER artifacts/
 
-# Get Kotlin since it is not available through apt
-# ===============================================
-mkdir -p artifacts/kotlin
-curl -L -o artifacts/kotlin/kotlinc.zip https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/kotlin-compiler-${KOTLIN_VERSION}.zip
-
-# FIXME(?): The "-linux-x64" version sounds correct but seems broken
-#curl -L -o artifacts/kotlin/kotlinc.zip https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/kotlin-compiler-${KOTLIN_VERSION}-linux-x64.zip
-
-# ===============================================
-
 
 # Build the actual problemtools images
 # ===============================================
@@ -51,6 +38,7 @@ for IMAGE in minimal icpc full; do
         sudo docker tag problemtools/${IMAGE}:${TAG} problemtools/${IMAGE}:latest
     fi
 done
+
 
 # Push to Docker Hub
 # ===============================================
