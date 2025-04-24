6d14502ecff421106a114557253a83054f199d3c
Gunnar Kreitz <gkreitz@kattis.com>
2025-04-24 14:02:59 +0200

    Hook sdist to make python -m build work on a clean checkout

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import subprocess
 
 import setuptools
 import setuptools.command.build
+import setuptools.command.sdist
 
 
 class BuildSupport(setuptools.Command):
@@ -21,9 +22,23 @@ class BuildSupport(setuptools.Command):
         self.set_undefined_options('build_py', ('build_lib', 'build_lib'))
 
     def run(self):
-        """Run command."""
         dest = os.path.join(os.path.realpath(self.build_lib), 'problemtools', 'support')
         command = ['make', '-C', 'support', 'install', 'DESTDIR=%s' % dest]
+        subprocess.check_call(command)
+
+class CheckoutChecktestdata(setuptools.Command):
+    """A custom command to build the support programs."""
+
+    description = 'checkout the git submodule for checktestdata (via make)'
+
+    def initialize_options(self) -> None:
+        pass
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self):
+        command = ['make', 'checktestdata']
         subprocess.check_call(command)
 
 # It's *very* unclear from setuptools' documentation what the best way to do this is.
@@ -33,14 +48,22 @@ class BuildSupport(setuptools.Command):
 # any documented way to inject a new subcommand (aside from overwriting one of
 # the existing `build_*`, but those are only run conditionally).
 class build(setuptools.command.build.build):
-    """Updated build command that also builds support."""
-
     def run(self):
         self.run_command('build_support')
+        super().run()
+
+# To make python -m build work from a fresh checkout, we also need to hook sdist to
+# do a git submodule checkout so that the source code for checktestdata is included
+# in the sdist (an alternative approach would be to include .git in the sdist (eww).
+class sdist(setuptools.command.sdist.sdist):
+    def run(self):
+        self.run_command('checkout_checktestdata')
         super().run()
 
 setuptools.setup(cmdclass={
           'build_support': BuildSupport,
           'build': build,
+          'checkout_checktestdata': CheckoutChecktestdata,
+          'sdist': sdist,
       },
 )
