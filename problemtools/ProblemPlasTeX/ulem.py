c1a36e026af2d0866134ab894c1c548c4c5a31dd
Gunnar Kreitz <gkreitz@kattis.com>
2025-05-09 16:36:51 +0200

    Ruff format

--- problemtools/ProblemPlasTeX/ulem.py
+++ problemtools/ProblemPlasTeX/ulem.py
@@ -1,11 +1,29 @@
 from plasTeX.Base.LaTeX.FontSelection import TextCommand
 
-class uline(TextCommand): pass
-class uuline(TextCommand): pass
-class uwave(TextCommand): pass
-class sout(TextCommand): pass
-class xout(TextCommand): pass
-class dashuline(TextCommand): pass
-class dotuline(TextCommand): pass
+
+class uline(TextCommand):
+    pass
 
 
+class uuline(TextCommand):
+    pass
+
+
+class uwave(TextCommand):
+    pass
+
+
+class sout(TextCommand):
+    pass
+
+
+class xout(TextCommand):
+    pass
+
+
+class dashuline(TextCommand):
+    pass
+
+
+class dotuline(TextCommand):
+    pass
