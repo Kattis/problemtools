8418e706afa3757114da1e5c8272f93216076841
Gunnar Kreitz <gkreitz@kattis.com>
2025-05-23 13:35:35 +0200

    Add script to build packages for pypi

---
+++ admin/build_pypi_packages.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+TAG=develop
+if [ "$1" != "" ]; then
+    TAG=$1
+fi
+
+cd $(dirname $(readlink -f $0))
+
+if ! ../venv/bin/twine -h > /dev/null 2> /dev/null; then
+    echo "Did not find twine. Please run ../venv/bin/pip install twine"
+    exit 1
+fi
+
+if [[ -n $(git status -s) ]]; then
+    echo "Repository is dirty."
+    git status -s
+    exit 1
+fi
+
+if [[ $(git rev-parse --abbrev-ref HEAD) != ${TAG} && $(git describe --exact-match --tags 2>/dev/null) != ${TAG} ]]; then
+    echo "Repository is currently not on branch/tag ${TAG}."
+    exit 1
+fi
+
+echo "Building sdist and manylinux wheel"
+sudo rm -rf ./pypi_dist
+docker run --rm -v $(pwd)/..:/problemtools -v $(pwd)/pypi_dist:/dist quay.io/pypa/manylinux_2_28_x86_64 /bin/bash -c "
+    yum -y install boost-devel gmp-devel ;
+    mkdir /build ;
+    cd /build ;
+    git config --global --add safe.directory /problemtools/.git ;
+    git clone /problemtools ;
+    cd problemtools ;
+    git checkout ${TAG} ;
+    /opt/python/cp311-cp311/bin/python -m build ;
+    auditwheel repair dist/problemtools-*.whl ;
+    cp dist/*.tar.gz /dist ;
+    cp wheelhouse/*.whl /dist"
+sudo chown -R $USER:$USER pypi_dist
+
+../venv/bin/twine check pypi_dist/*
+
+echo "Running verifyproblem from wheel on all examples"
+TEMPDIR=$(mktemp -d)
+python3 -m venv "${TEMPDIR}"
+"${TEMPDIR}/bin/pip" install pypi_dist/problemtools*manylinux*whl
+shopt -s extglob
+if ! "${TEMPDIR}/bin/verifyproblem" ../examples/!(README.md); then
+    echo "Running verifyproblem on all examples failed. Please review output above to debug."
+    rm -rf "${TEMPDIR}"
+    exit 1
+fi
+rm -rf "${TEMPDIR}"
+
+echo "Sucessfully built packages. If you're happy with them, upload:"
+echo "    ../venv/bin/twine upload --verbose pypi_dist/*"
