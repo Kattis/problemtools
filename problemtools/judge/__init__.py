18e883f5ea13f20f09eee573e01a1f9969aa4ab8
Gunnar Kreitz <gkreitz@kattis.com>
2026-04-23 22:25:26 +0200

    Add functions in judge to execute one test case

--- problemtools/judge/__init__.py
+++ problemtools/judge/__init__.py
@@ -1,6 +1,9 @@
+from .execute import execute_testcase
 from .result import (
     SubmissionResult,
+    TimeLimits,
     Verdict,
+    classify_result,
     is_RTE,
     is_TLE,
 )
@@ -8,7 +11,10 @@ from .validate import validate_output
 
 __all__ = [
     'SubmissionResult',
+    'TimeLimits',
     'Verdict',
+    'classify_result',
+    'execute_testcase',
     'is_RTE',
     'is_TLE',
     'validate_output',
