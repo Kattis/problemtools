808a071fc8912934e0997c18c786a61bda7fa54b
Gunnar Kreitz <gkreitz@kattis.com>
2026-08-26 15:17:45 +0200

    Create model for TestCase and TestDataGroup

--- problemtools/model/__init__.py
+++ problemtools/model/__init__.py
@@ -5,6 +5,7 @@ Verdict = Literal['AC', 'TLE', 'OLE', 'MLE', 'RTE', 'WA', 'PAC', 'JE']
 
 from .includes import DEFAULT_LANGUAGE, IncludeFile, Includes, LanguageIncludes, load_includes
 from .submissions import LegacyPolicy, Submission, Submissions, load_submissions
+from .testdata import TestCase, TestDataGroup, load_testdata
 
 __all__ = [
     'DEFAULT_LANGUAGE',
@@ -14,7 +15,10 @@ __all__ = [
     'LegacyPolicy',
     'Submission',
     'Submissions',
+    'TestCase',
+    'TestDataGroup',
     'Verdict',
     'load_includes',
     'load_submissions',
+    'load_testdata',
 ]
