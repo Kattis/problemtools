1e5ab48fc38bf7eb5d9651e76cd0c51eb7f0538a
Gunnar Kreitz <gkreitz@kattis.com>
2026-08-27 16:47:11 +0200

    Move problem package checks to checks module too

--- problemtools/verifyproblem.py
+++ problemtools/verifyproblem.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import argparse
-import difflib
 import logging
 import os
 import random
@@ -524,9 +523,7 @@ class Problem(ProblemAspect):
             if self.format is FormatVersion.V_2023_07:
                 self.warning(f'Support for version {self.format} is very incomplete. Verification may not work as expected.')
 
-            self._check_symlinks()
-            self._check_file_and_directory_names()
-            self._check_submission_directory_names()
+            checks.check_problem_package(Path(self.probdir), self.format, self._diag)
 
             run.limit.check_limit_capabilities(self._diag)
 
@@ -554,77 +551,6 @@ class Problem(ProblemAspect):
             # the directory tree it uses.
             context.wait_for_background_work()
         return self.errors, self.warnings
-
-    def _check_submission_directory_names(self) -> None:
-        """Heuristically check if submissions contain any directories that will be ignored because of typos or format mismatches"""
-        submission_directories = [p.name for p in (Path(self.probdir) / 'submissions').glob('*') if p.is_dir()]
-        if len(submission_directories) == 0:
-            return
-
-        def most_similar(present_dir: str, format_version: FormatVersion) -> tuple[str, float]:
-            similarities = [
-                (spec_dir, difflib.SequenceMatcher(None, present_dir, spec_dir).ratio())
-                for spec_dir in format_version.submission_directories
-            ]
-            return max(similarities, key=lambda x: x[1])
-
-        for present_dir in submission_directories:
-            most_similar_dir, max_similarity = most_similar(present_dir, self.format)
-
-            if max_similarity == 1:
-                # Exact match, no typo
-                continue
-
-            if 0.75 <= max_similarity:
-                self.warning(f'Potential typo: directory submissions/{present_dir} is similar to {most_similar_dir}')
-            else:
-                for other_version in [v for v in FormatVersion if v != self.format]:
-                    _, max_similarity = most_similar(present_dir, other_version)
-                    if max_similarity == 1:
-                        self.warning(
-                            f'Directory submissions/{present_dir} is not part of format version {self.format}, but part of {other_version}'
-                        )
-                        break
-
-    def _check_symlinks(self) -> None:
-        """Check that all symlinks point to something existing within the problem package"""
-        probdir = os.path.realpath(self.probdir)
-        for root, dirs, files in os.walk(probdir):
-            for file in dirs + files:
-                filename = os.path.join(root, file)
-                if os.path.islink(filename):
-                    target = os.path.realpath(filename)
-                    # relfile is the filename of the symlink, relative to the problem root (only used for nicer error messages)
-                    relfile = os.path.relpath(filename, self.probdir)
-                    # reltarget is what the symlink points to (absolute, or relative to where the symlink is)
-                    reltarget = os.readlink(filename)
-                    if not os.path.exists(target):
-                        self.error(f'Symlink {relfile} links to {reltarget} which does not exist')
-                    if os.path.commonpath([probdir, target]) != probdir:
-                        self.error(f'Symlink {relfile} links to {reltarget} which is outside of problem package')
-                    if os.path.isabs(reltarget):
-                        self.error(
-                            f'Symlink {relfile} links to {reltarget} which is an absolute path. Symlinks must be relative.'
-                        )
-
-    def _check_file_and_directory_names(self) -> None:
-        regex = re.compile(r'^[a-zA-Z0-9_][a-zA-Z0-9_.-]{0,254}$')
-
-        def _special_case_allowed_files(file: str, reldir: str) -> bool:
-            return file == '.gitignore' or (file == '.timelimit' and reldir == os.path.basename(self.probdir))
-
-        def _special_case_allowed_dirs(directory: str, reldir: str) -> bool:
-            return directory == '.git' and reldir == os.path.basename(self.probdir)
-
-        for root, dirs, files in os.walk(self.probdir):
-            # Path of the directory we're in, starting with problem shortname. Only used for nicer error messages.
-            reldir = os.path.relpath(root, os.path.dirname(self.probdir))
-            for file in files:
-                if not regex.match(file) and not _special_case_allowed_files(file, reldir):
-                    self.error(f"Invalid file name '{file}' in {reldir}, should match {regex.pattern}")
-            for directory in dirs:
-                if not regex.match(directory) and not _special_case_allowed_dirs(directory, reldir):
-                    self.error(f"Invalid directory name '{directory}' in {reldir}, should match {regex.pattern}")
 
 
 def re_argument(s: str) -> Pattern[str]:
